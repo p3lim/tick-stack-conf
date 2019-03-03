7bb7769fd4aa2f6af0bd33b68a5482252fb7dc58
Adrian L Lange <mail@p3lim.net>
2019-03-03 11:33:08 +0100

    Manage r10k once we're set up

--- site/profile/manifests/r10k.pp
+++ site/profile/manifests/r10k.pp
@@ -1,6 +1,16 @@
 class profile::r10k {
-	# just make sure r10k pulls every 30 minutes
+	class { 'r10k':
+		version => latest,
+		sources => {
+			'puppet' => {
+				'remote'  => 'https://github.com/p3lim/tick-stack-conf.git',
+				'basedir' => '/etc/puppetlabs/code/environments',
+				'prefix'  => false,
+			},
+		},
+	}
 
+	# just make sure r10k pulls every 30 minutes
 	cron { 'r10k':
 		command => '/bin/r10k deploy environment -p',
 		user    => 'root',
